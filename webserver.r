5d231e94bccd23588f1ec3949d425dbb0d3864d5
cblicous <cbelka@gmail.com>
2017-03-18 14:35:43 +0100

    Create webserver.r

---
+++ webserver.r
@@ -0,0 +1,32 @@
+# first steps building a tiny rook based json service
+# with code i googled together
+# need to install rook 
+
+library(Rook)
+library(jsonlite)
+
+rook <- Rhttpd$new()
+rook$start(quiet=TRUE)
+rook$print()
+
+
+rook = Rhttpd$new()
+rook$add(
+  name ="summarize",
+  app  = function(env) {
+    req = Rook::Request$new(env)
+  
+    numbers =  c(3,4,2)
+    
+    results = list()
+    results$mean = mean(numbers)
+    results$sd = sd(numbers)
+    results$numbers = numbers
+
+    res = Rook::Response$new()
+    res$write(toJSON(results))
+    res$finish()
+  }
+)
+
+rook$browse("summarize")
